3f715a1f82b9fe3c56fd2671323e586f0b25b5bb
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-10-14 16:19:33 +0530

    Created few more api's to update and fetch details

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.example.accounts.controller;
 import com.example.accounts.constants.AccountsConstants;
 import com.example.accounts.dto.CustomerDTO;
 import com.example.accounts.dto.ResponseDTO;
+import com.example.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,36 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO){
+        iAccountsService.createAccount(customerDTO);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam String mobileNumber){
+        CustomerDTO customerDTO = iAccountsService.fetchAccount(mobileNumber);
+        return new ResponseEntity<>(customerDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateAccountDetails(@RequestBody CustomerDTO customerDto){
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDTO(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
     }
 }
