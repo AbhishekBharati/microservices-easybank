e2cb8dc7a073bf08b1df7f22af995c42e8466d90
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-10-17 10:27:22 +0530

    Added GlobalExceptionHandler and Validation for CardsDTO

--- cards/src/main/java/com/example/cards/controllers/CardsController.java
+++ cards/src/main/java/com/example/cards/controllers/CardsController.java
@@ -4,22 +4,25 @@ import com.example.cards.constants.CardsConstants;
 import com.example.cards.dto.CardsDto;
 import com.example.cards.dto.ResponseDto;
 import com.example.cards.service.ICardsService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class CardsController {
 
     private ICardsService iCardsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createNewCard(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> createNewCard(@Valid @RequestParam String mobileNumber){
         iCardsService.createCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
@@ -32,7 +35,7 @@ public class CardsController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateCard(@RequestBody CardsDto cardsDto){
+    public ResponseEntity<ResponseDto> updateCard(@Valid @RequestBody CardsDto cardsDto){
         boolean isUpdated = iCardsService.updateCard(cardsDto);
         if(isUpdated){
             return ResponseEntity.status(HttpStatus.OK)
