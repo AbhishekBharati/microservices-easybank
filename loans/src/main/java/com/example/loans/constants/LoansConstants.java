7247ccf8b4a0d8dc640f2288b9a27d18da647688
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-10-20 17:49:17 +0530

    Added constants that are going to be used in application

---
+++ loans/src/main/java/com/example/loans/constants/LoansConstants.java
@@ -0,0 +1,17 @@
+package com.example.loans.constants;
+
+public final class LoansConstants {
+    private LoansConstants(){
+//        Restricting Initialization :-
+    }
+
+    public static final String HOME_LOAN = "Home Loan";
+    public static final int NEW_LOAN_LIMIT = 1_00_000;
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Loan Created Successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request Processed Successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Message Updation failed. Please try again later or contact dev team";
+    public static final String MESSAGE_417_DELETE = "Message Deletion failed. Please try again later or contact dev team";
+}
