93ce758131f6af43143a637fd0dacd55264b3a87
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-10-28 18:50:27 +0530

    Added the logic of getting Enviornment Variables using @EnvironmentalConfigurations

--- loans/src/main/java/com/example/loans/controllers/LoansController.java
+++ loans/src/main/java/com/example/loans/controllers/LoansController.java
@@ -1,10 +1,12 @@
 package com.example.loans.controllers;
 
 import com.example.loans.constants.LoansConstants;
+import com.example.loans.dto.LoansContactInfoDto;
 import com.example.loans.dto.LoansDto;
 import com.example.loans.dto.ResponseDto;
 import com.example.loans.service.ILoansService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ import java.awt.*;
 @AllArgsConstructor
 public class LoansController {
     private ILoansService iLoansService;
+
+    @Autowired
+    private LoansContactInfoDto loansContactInfoDto;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createNewLoan(@RequestParam String mobileNumber){
@@ -49,5 +54,11 @@ public class LoansController {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @GetMapping("/get-contact")
+    public ResponseEntity<LoansContactInfoDto> getContactInfo(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(loansContactInfoDto);
     }
 }
