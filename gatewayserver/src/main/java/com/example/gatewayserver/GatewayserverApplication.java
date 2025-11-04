a9f58efbe4f16c430d3d028492e8097a0354c523
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-11-04 17:30:59 +0530

    Added our custom routes to point to microservices

--- gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -9,5 +14,27 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
+
+    @Bean
+    public RouteLocator eazyBankROuteConfig(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/easybank/accounts/**")
+                        .filters(f ->
+                                f.rewritePath("/easybank/accounts/(?<segment>.*)","/${segment}")
+                                        .addResponseHeader("X-Resoponse-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/easybank/loans/**")
+                        .filters(f -> f.rewritePath("/easybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/easybank/cards/**")
+                        .filters(f -> f.rewritePath("/easybank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS"))
+                .build();
+    }
 
 }
