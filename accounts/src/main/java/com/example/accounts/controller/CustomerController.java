b90d81c9d1a29543525a648cc64e5a76d576dd53
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-11-04 17:25:47 +0530

    Added the correlation-id to track each and everyrequest

--- accounts/src/main/java/com/example/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/example/accounts/controller/CustomerController.java
@@ -2,17 +2,18 @@ package com.example.accounts.controller;
 
 import com.example.accounts.dto.CustomerDetailsDto;
 import com.example.accounts.service.ICustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private ICustomerService iCustomerService;
     public CustomerController(ICustomerService iCustomerService){
@@ -20,8 +21,10 @@ public class CustomerController {
     }
 
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam String mobileNumber){
-        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber);
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails( @RequestHeader("eazybank-correlation-id") String correlationId,
+            @RequestParam String mobileNumber){
+        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(customerDetailsDto);
     }
